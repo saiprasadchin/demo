d37cbc18973868f269e4ff65aea9306f0156f4a3
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-05-19 08:41:05 -0700

    second UC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,10 +3,16 @@ echo "Welcome to Employee Wage Computation program"
 
 present=1
 absent=0
+empRatePerH=20
 attend=$(( RANDOM%2 ))
 if [ $attend == 1 ]
 then
 	echo "Employee present"
+	empH=8
 else
 	echo "Employee absent"
+	empH=0
 fi
+
+sal=$(($empH * $empRatePerH))
+echo "Employ day salary is : $sal"
