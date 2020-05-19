c3e611bf65b4ac9c6698e47e8e723db0bf4801a0
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-05-19 08:00:39 -0700

    first UC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,12 @@
 #! /bin/bash -x
 echo "Welcome to Employee Wage Computation program"
+
+present=1
+absent=0
+attend=$(( RANDOM%2 ))
+if [ $attend == 1 ]
+then
+	echo "Employee present"
+else
+	echo "Employee absent"
+fi
