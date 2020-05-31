418e850f3effe78a0d2a2744a6907b84100c91a5
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-05-31 06:25:14 -0700

    EmployeeUC1

---
+++ Employee.java
@@ -0,0 +1,18 @@
+/*class Employee{
+	public static void main(String[] args){
+		System.out.println("Welcome to Employee Wage Computation");
+	}
+}*/
+
+
+class Employee{
+
+        public static void main(String[] args){
+                int ispresent= (int) (Math.random()*10%2);
+		if(ispresent==1){
+			 System.out.println("Employee is Present");
+		}else{
+			 System.out.println("Employee is Absent");
+		}
+        }
+}
