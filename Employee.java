2ac897c6ea6c3f673f399092abc421983d992f11
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-01 01:40:12 -0700

    UC2

--- Employee.java
+++ Employee.java
@@ -1,18 +1,18 @@
-/*class Employee{
-	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation");
-	}
-}*/
 
-
-class Employee{
-
+public class Employee{
         public static void main(String[] args){
-                int ispresent= (int) (Math.random()*10%2);
-		if(ispresent==1){
-			 System.out.println("Employee is Present");
-		}else{
-			 System.out.println("Employee is Absent");
-		}
+                System.out.println("Welcome to Employee Wage Computation Program\n");
+                int attendance = (int)((Math.random() * 10) % 2);
+                System.out.println((attendance == 1) ? "Employee Present" : "Employee Absent");
+                int work_hours = 8;
+                int wage = daily_wage(attendance, work_hours);
+                System.out.print("Day's wage is :"+wage);
         }
+        public static int daily_wage(int attendance, int work_hours){
+                int wage = 0;
+                if(attendance == 1){
+                        wage = 20 * work_hours;
+                }
+                return wage;
+    }
 }
