f7b24288023acc2320caf853c5807b0eb9ff0add
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-01 01:57:03 -0700

    UC4

--- Employee.java
+++ Employee.java
@@ -1,28 +1,36 @@
 public class Employee{
-        public static void main(String[] args){
-                System.out.println("Welcome to Employee Wage Computation Program\n");
-                int attendance = (int)((Math.random() * 10) % 2);
-                System.out.println((attendance == 1) ? "Employee Present" : "Employee Absent");
-                int workTime = 0;
-
-                if(attendance == 1)
-                    workTime = (int)((Math.random() * 10) % 2);
-
-                int work_hours = 0;
-
-                if(workTime == 1)
-                    work_hours = 8;
-                else
-                    work_hours = 4;
-
-                int wage = 0;
-
-                if(attendance == 1){
-                    System.out.println((workTime == 1) ? "Full time" : "Part time");
-                    wage = 20 * work_hours;
-                }
-                System.out.print("Day's wage is :"+wage);
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation Program\n");
+        int attendance = (int)((Math.random() * 10) % 2);
+        int isPresent = 1;
+        int isAbsent = 0;
+        int isFullTime = 1;
+        int isPartTime = 2;
+        int empHours = 0;
+        int empRatePerHour = 20;
+        int salary = 0;
+        if( attendance == 1){
+            System.out.println("Employee Present");
+            empHours = 8;
         }
-
+        else {
+            System.out.println("Employee Absent");
+        }
+        if(attendance == 1){
+            int time =(int)((Math.random() * 10 ) % 3);
+            switch (time) {
+                case 1:
+                    System.out.println("Full Time");
+                    break;
+                case 2:
+                    System.out.println("Part Time");
+                    empHours=4;
+                    break;
+                default:
+                    break;
+            }
+            salary = (empHours * empRatePerHour);
+            System.out.println("Salary is:"+salary);
+        }
+    }
 }
-
